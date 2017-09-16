87805b8a03a84c469fd4536ccdc2e38ef81be13b
Leon <redzicleon@gmail.com>
2017-09-16 11:31:23 +0200

    false

--- web/index.php
+++ web/index.php
@@ -16,57 +16,106 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
+// 501 Failed mac address check
+// 502 Missing parameters in request
+// 503 No Status file
+// 504 No Data file
 
-// Our web handlers
+// 200 Ok
+// 201 Needs approval of ip change
 
-$app->get('/', function(Request $request) use($app) {
-  $app['monolog']->addDebug('logging output.');
-  $parametri = $request->query->all();
-  if(!is_null(@$parametri['mac'])){
-    // CHECK THE MAC Address
-    if(!is_null(@$parametri['ip']) ) {
-      // CHECK THE IP
+// 303 Logic Error
 
-    }
-  }
-  file_put_contents("data.json", json_encode(array('mac' => 'ab:ab:ga:bc:de:da:ba', 'ip' => '192.168.1.1', 'network_id' => 'homeinternet') ) );
+// 404 Pi not logged in
 
+/* 
+ * Functions 
+ */
+function Respond( $data = null , $code = 303 ) {
+  if(is_null($data)) $data = array("status" => $code);
   $response = new Response(
-    json_encode(array("status" => "ok") ),
+    json_encode( $data ),
     Response::HTTP_OK,
     array('content-type' => 'application/json')
   );
-
   return $response;
-});
-// 501 Failed mac address check
-// 502 Missing parameters in request
+}
+function Status() {
+  try{
+    $status_json = @file_get_contents("status.json");
+    $status = json_decode($status_json, true);
+  } catch(Exception $e) {
+    $status = false;
+  }
+  if(!$status)
+    file_put_contents("status.json", json_encode( array("status" => 504) ) );
+  return $status;
+}
+function Data(){
+  try{
+    $data_json = @file_get_contents("data.json");
+    $data = json_decode($data_json, true);
+  } catch(Exception $e) {
+    $data = false;
+  }
+  if(!$data){
+    file_put_contents("status.json", json_encode( array("status" => 504) ) );
+  }
+  return $data;
+}
 
-// 201 Needs approval of ip change
+/* 
+ * End Functions 
+ */
+
+
+// Our web handlers
+
+// TODO Ovdje ide view za obavještavanje i to sve živo
+$app->get('/', function(Request $request) use($app) {
+  $app['monolog']->addDebug('logging output.');
+  $status = Status();
+  if(!$status) return Respond(null, 503);
+
+  switch($status["status"]){
+    case 201:
+      $data = Data();
+      if(!$data) return Respond( array('status'=>404) );
+      $response_data = $status;
+      $response_data["data"] = $data;
+
+      return $app->redirect("http://".$data["ip"]);
+    break;
+    case 504:
+      // No data file view
+      return Respond($status);
+    default:
+      return Respond($status);
+    break;
+  }
+
+  return $app->redirect("http://www.google.com");
+});
+
 $app->post('/', function(Request $request) use($app) {
   $app['monolog']->addDebug('logging output.');
   $data = $request->request->all();
   if(!is_null(@$data['mac'])){
-    if($data['mac'] == "b8:27:eb:cc:02:a4") {
+    if($data['mac'] == "") {
       // Prošla provjera mac adrese
       if(!is_null(@$data['ip']) ) {
         if(!is_null(@$data["ssid"])){
             // Prošlo sve provjere zapiši u temp file i status treba biti da se treba odobriti promjena ip adrese mail ili interface?
+            unset($data["mac"]);
             file_put_contents("data.json", json_encode( $data ) );
             file_put_contents("status.json", json_encode( array("status" => 201) ) );
-        } else $data = array('status' => 'failed', 'code' => 502);
-      } else $data = array('status' => 'failed', 'code' => 502);
-    } else $data = array('status' => 'failed', 'code' => 501);
-  } else $data = array('status' => 'failed', 'code' => 502);
-  
+            $data = array( 'status' => 200 );
+        } else $data = array('status' => 502);
+      } else $data = array('status' => 502);
+    } else $data = array('status' => 501);
+  } else $data = array('status' => 502);
 
-  $response = new Response(
-    json_encode(array("status" => "ok") ),
-    Response::HTTP_OK,
-    array('content-type' => 'application/json')
-  );
-
-  return $response;
+  return Respond( $data );
 
 });
 
@@ -74,24 +123,26 @@ $app->get('/discover', function(Request $request) use($app) {
   $app['monolog']->addDebug('logging output.');
 
   $response_data = array();
-  $status_json = file_get_contents("status.json");
-  $status = json_decode($status_json);
+  $status = Status();
+  if(!$status) return Respond(null, 503);
+
   switch($status["status"]){
     case 201:
-      $response_data = $status;
+      return Respond( $status );
     break;
     case 200:
+      $data = Data();
+      if(!$data) return Respond( array('status'=>404) );
       $response_data = $status;
-      $response_data["data"] = json_encode("data.json");
+      $response_data["data"] = $data;
+
+      return Respond( $response_data );
+    default:
+      return Respond($status);
   }
-
-  $response = new Response(
-    json_encode( $response_data ),
-    Response::HTTP_OK,
-    array('content-type' => 'application/json')
-  );
-
-  return $response;
+});
+$app->get("/test", function() use($app){
+  return var_dump( getenv("RPI_MAC", true) );
 });
 
 $app->run();
